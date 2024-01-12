e3c051b464748ebbbadac095587962f8c9bc54a1
royfredeluces95 <roy.fredeluces@sence1.com>
2024-01-12 22:20:42 +0800

    added routes for subscribers controller

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :subscribers
+  post 'subscribers/soft_delete', to: 'subscribers#soft_del'
+  post 'subscribers/restore', to: 'subscribers#restore'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
