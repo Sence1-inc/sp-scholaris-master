6efdf2a5d7e2a39d60bdf4e088fa05af69025fd9
roandayne <roan.dino@sence1.com>
2024-01-12 16:41:19 +0800

    Restore the methods that were originally included in the scaffold

--- backend/app/controllers/subscribers_controller.rb
+++ backend/app/controllers/subscribers_controller.rb
@@ -1,6 +1,25 @@
 class SubscribersController < ApplicationController
   skip_before_action :verify_authenticity_token
+
+  # GET /subscribers
+  def index
+    @subscribers = Subscriber.all
+  end
+
+  # GET /subscribers/{id}
+  def show
+  end
+
+  # GET /subscribers/new
+  def new
+    @post = Post.new
+  end
+
+  # GET /subscribers/{id}/edit
+  def edit
+  end
   
+  # POST /subscribers
   def create
     @subscriber = Subscriber.new(subscriber_params)
     if EmailValidator.valid?(subscriber_params[:email])
@@ -12,6 +31,14 @@ class SubscribersController < ApplicationController
     else
       render json: { error: "Subscription failed", details: "The email '#{subscriber_params[:email]}' is not valid." }, status: 400
     end
+  end
+
+  # PATCH/PUT /subscribers/{id}
+  def update
+  end
+
+  # DELETE /subscribers/{id}
+  def destroy
   end
 
   private
