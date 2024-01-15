0aea3e938e0a1ff87898ba9d22a620a150b821bb
roandayne <roan.dino@sence1.com>
2024-01-15 12:00:07 +0800

    Add namespace to accomodate /api/v1 in routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :subscribers
+  namespace :api do
+    namespace :v1 do
+      resources :subscribers
+    end
+  end
 end
