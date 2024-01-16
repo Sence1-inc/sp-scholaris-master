e07c307d3795e76f1973ba38e8ac2d897f162f2d
royfredeluces95 <roy.fredeluces@sence1.com>
2024-01-16 15:42:35 +0800

    added model methods for soft delete and restore

--- backend/app/models/subscriber.rb
+++ backend/app/models/subscriber.rb
@@ -2,14 +2,11 @@ class Subscriber < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :user_type, presence: true, inclusion: { in: %w(provider student) }
 
-    # scope :soft_delete, -> { where("LENGTH(title) > ?", length) }
-
     def self.soft_delete(obj)
-    # abort(obj.unsubscribed_at)
-    obj.update(unsubscribed_at:Time.now) if obj.unsubscribed_at == nil
+        obj.update(deleted_at:Time.now) if obj.deleted_at == nil
     end
 
     def self.restore(obj)
-        obj.update(unsubscribed_at:nil) if obj.unsubscribed_at.present?
+        obj.update(deleted_at:nil) if obj.deleted_at.present?
     end
 end
