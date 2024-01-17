118a9222d75701e3b46e535ff449fabd9d1dcb22
royfredeluces95 <roy.fredeluces@sence1.com>
2024-01-17 15:31:53 +0800

    removed the soft_delete methods in Subscriber Model

--- backend/app/models/subscriber.rb
+++ backend/app/models/subscriber.rb
@@ -1,16 +1,5 @@
-class Subscriber < ApplicationRecord
+class Subscriber < ApplicationRecord    
     validates :email, presence: true, uniqueness: true
     validates :user_type, presence: true, inclusion: { in: %w(provider student) }
 
-    def self.is_unsubscribed(obj)
-        !obj.deleted_at.present?
-    end
-
-    def self.soft_delete(obj) 
-        obj.update(deleted_at:Time.now) if obj.deleted_at == nil
-    end
-
-    def self.restore(obj)
-        obj.update(deleted_at:nil) if obj.deleted_at.present?
-    end
 end
