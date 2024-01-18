d5b106901e3396c9eec624bb3d5e17a78f49be59
royfredeluces95 <roy.fredeluces@sence1.com>
2024-01-18 16:16:56 +0800

    revised spacing

--- backend/app/models/subscriber.rb
+++ backend/app/models/subscriber.rb
@@ -1,5 +1,5 @@
 class Subscriber < ApplicationRecord    
-    validates :email, presence: true, uniqueness: true
-    validates :user_type, presence: true, inclusion: { in: %w(provider student) }
+  validates :email, presence: true, uniqueness: true
+  validates :user_type, presence: true, inclusion: { in: %w(provider student) }
 
 end
