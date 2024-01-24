d98e9dc0235a0e1b01eb56e1fd2eafc3b81fc08d
christian-barrios995 <christian.barrios@sence1.com>
2024-01-24 13:17:35 +0800

    fixed /privacy-concent typo

--- frontend/src/containers/TeaserPage/TeaserProvider.jsx
+++ frontend/src/containers/TeaserPage/TeaserProvider.jsx
@@ -10,7 +10,7 @@ function HomePage() {
     <>
       <Header />
       <WelcomeSection />
-      <Newsletter title_content={'Stay Updated from our Newsletters'} subtitle_content={'Join Now, Stay Informed'} description_content={'Unlock Exclusive Updates for a Brighter Tomorrow.'} consent_content={<>'By subscribing to the newsletter, I have read this form and understand its content and voluntarily give my consent for the collection, use, processing, storage and retention of my personal data or information to Sence1 for the purpose(s) described in the{" "}<Link to={'/privacy-concent'}>Privacy Policy</Link>{" "}document'</>}/>
+      <Newsletter title_content={'Stay Updated from our Newsletters'} subtitle_content={'Join Now, Stay Informed'} description_content={'Unlock Exclusive Updates for a Brighter Tomorrow.'} consent_content={<>'By subscribing to the newsletter, I have read this form and understand its content and voluntarily give my consent for the collection, use, processing, storage and retention of my personal data or information to Sence1 for the purpose(s) described in the{" "}<Link to={'/privacy-consent'}>Privacy Policy</Link>{" "}document'</>}/>
       <Footer />
     </>
   );
