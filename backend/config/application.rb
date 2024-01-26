33d481262177a0135e0087d2d43541a6811c7609
roandayne <roan.dino@sence1.com>
2024-01-26 10:12:18 +0800

    Update port

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -24,6 +24,6 @@ module Backend
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+    config.action_mailer.default_url_options = { host: 'localhost', port: 5001 }
   end
 end
