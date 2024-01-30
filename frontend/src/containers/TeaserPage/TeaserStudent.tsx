78a37c8f7cd13aa853f3e6c05d2e425d8d476a37
roandayne <roan.dino@sence1.com>
2024-01-30 15:27:41 +0800

    Minore updates for types, css, and content

--- frontend/src/containers/TeaserPage/TeaserStudent.tsx
+++ frontend/src/containers/TeaserPage/TeaserStudent.tsx
@@ -2,18 +2,19 @@ import React from 'react';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 import FeatureGuides from '../../components/Feature/FeatureGuides';
-import { FEATURES, PROVIDER_WELCOME_SUBHEADER } from '../../data/ProviderContent';
+import { FEATURES } from '../../data/ProviderContent';
 import WelcomeSection from '../../components/WelcomeSection/WelcomeSection';
 import Newsletter from '../../components/Newsletter/Newsletter';
-import { PROVIDER_TYPE } from '../../constants/constants';
+import { STUDENT_TYPE } from '../../constants/constants';
+import { STUDENT_WELCOME_SUBHEADER, STUDENT_WELCOME_THIRD_LEVEL_HEADING } from '../../data/StudentContent';
 
 const HomePage: React.FC = () => {
   return (
     <>
       <Header />
-      <WelcomeSection subheader={PROVIDER_WELCOME_SUBHEADER} />
+      <WelcomeSection subheader={STUDENT_WELCOME_SUBHEADER} third_level_header={STUDENT_WELCOME_THIRD_LEVEL_HEADING} />
       <FeatureGuides features={FEATURES} />
-      <Newsletter user_type={PROVIDER_TYPE} title_content={'Stay Updated from our Newsletters'} subtitle_content={'Join Now, Stay Informed'} description_content={'Unlock Exclusive Updates for a Brighter Tomorrow.'} />
+      <Newsletter user_type={STUDENT_TYPE} title_content={'Stay Updated from our Newsletters'} subtitle_content={'Join Now, Stay Informed'} description_content={'Unlock Exclusive Updates for a Brighter Tomorrow.'} />
       <Footer />
     </>
   );
