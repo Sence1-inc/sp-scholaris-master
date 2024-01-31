581e8ef45ed054c2ae1c10656205e30dc0bd3a04
sdidulosence1 <sarah.didulo@sence1.com>
2024-01-31 16:55:36 +0800

    resolved conflict in teaserstudent

--- frontend/src/containers/TeaserPage/TeaserStudent.tsx
+++ frontend/src/containers/TeaserPage/TeaserStudent.tsx
@@ -3,6 +3,8 @@ import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 import WelcomeSection from '../../components/WelcomeSection/WelcomeSection';
 import Newsletter from '../../components/Newsletter/Newsletter';
+import FeatureGuides from '../../components/Feature/FeatureGuides';
+import { FEATURES } from '../../data/StudentContent';
 import { STUDENT_TYPE } from '../../constants/constants';
 import { STUDENT_WELCOME_SUBHEADER, STUDENT_WELCOME_THIRD_LEVEL_HEADING } from '../../data/StudentContent';
 import Search from '../../components/Search/Search';
@@ -12,6 +14,7 @@ const HomePage: React.FC = () => {
     <>
       <Header />
       <WelcomeSection subheader={STUDENT_WELCOME_SUBHEADER} third_level_header={STUDENT_WELCOME_THIRD_LEVEL_HEADING} />
+      <FeatureGuides features={FEATURES} contentType='studentFeatures'/>
       <Newsletter user_type={STUDENT_TYPE} title_content={<>
             Hear the latest from{' '}
             <span className='newsletter-subheader__orange'>Scholaris</span>
