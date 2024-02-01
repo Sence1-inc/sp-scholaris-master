597b02e02fc64478be52e242d0ea7cd8149e32cb
roandayne <roan.dino@sence1.com>
2024-02-02 01:53:06 +0800

    Use redux to store scholarships and params

---
+++ frontend/src/redux/reducers/ScholarshipsReducer.ts
@@ -0,0 +1,23 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import store from "../store";
+import { Params, Scholarship } from "../types";
+
+interface Scholarships {
+  scholarships: Scholarship[];
+}
+
+export const scholarshipsSlice: any = createSlice({
+  name: "scholarships",
+  initialState: {
+    scholarships: [],
+  } as Scholarships,
+  reducers: {
+    initializeScholarships: (state, action: PayloadAction<Scholarship[]>) => {
+      state.scholarships = action.payload;
+    },
+  },
+});
+
+export const { initializeScholarships } = scholarshipsSlice.actions;
+
+export default scholarshipsSlice.reducer;
---
+++ frontend/src/redux/store.ts
@@ -0,0 +1,21 @@
+import { configureStore } from '@reduxjs/toolkit'
+import ScholarshipsReducer from './reducers/ScholarshipsReducer'
+import SearchParamsReducer from './reducers/SearchParamsReducer'
+import { TypedUseSelectorHook, useSelector, useDispatch } from "react-redux";
+
+const store = configureStore({
+  reducer: {
+    scholarships: ScholarshipsReducer,
+    searchParams: SearchParamsReducer,
+  },
+})
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export default store;
