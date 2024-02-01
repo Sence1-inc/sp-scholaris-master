1b371a6c9896df0a84a94f1a8a492f438703fc5a
christian-barrios995 <christian.barrios@sence1.com>
2024-02-01 11:59:24 +0800

    ui change

--- frontend/src/containers/SearchResultsPage/SearchResultsPage.tsx
+++ frontend/src/containers/SearchResultsPage/SearchResultsPage.tsx
@@ -5,6 +5,8 @@ import SearchInput from "../../components/SearchInput/SearchInput";
 import Button from "../../components/Button/Button";
 import { Link } from "react-router-dom";
 
+import viewSvg from '../../public/images/view.svg'
+
 import "./SearchResultsPage.css";
 
 export default function SearchResultsPage() {
@@ -23,19 +25,30 @@ export default function SearchResultsPage() {
           </div>
           <div className="search__results-container">
             <div className="search__results-header">
-              <h4>Scholarship Details</h4>
+              <p>Scholarship Details</p>
+              <p>Application Start Date</p>
+              <p>Application Due Date</p>
+              <p>Provider</p>
               <p>Actions</p>
             </div>
             <ul>
               <li className="search__results-content">
                 <p className="search__results-item">
-                  Tulong Dunong Scholarship <span>Ched Scholarship</span>{" "}
+                  Tulong Dunong Scholarship 
                 </p>
-                <div>
+                <p className="search__results-item">
+                  01/20/2024
+                </p>
+                <p className="search__results-item">
+                  01/20/2025
+                </p>
+                <p className="search__results-item">
+                  CHED
+                </p>
                   <Link to={"/"} className="seach__results-link">
-                    View
+                    View 
+                    <img src={viewSvg} alt="View icon" />
                   </Link>
-                </div>
               </li>
             </ul>
           </div>
