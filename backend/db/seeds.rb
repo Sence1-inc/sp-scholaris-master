329de4d52f848c6a56395fe985aabff3bb4144c6
roandayne <roan.dino@sence1.com>
2024-02-02 12:08:57 +0800

    Execute statements one by one

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,5 +1,10 @@
-sql_file_path = Rails.root.join('./db/mock-data.sql')
-sql = File.read(sql_file_path)
-ActiveRecord::Base.connection.execute(sql)
+sql_file_path = Rails.root.join('db', 'mock-data.sql')
+sql_statements = File.read(sql_file_path)
+
+individual_statements = sql_statements.split(';').map(&:strip).reject(&:empty?)
+
+individual_statements.each do |statement|
+  ActiveRecord::Base.connection.execute("#{statement};")
+end
 
 puts 'Seed data loaded successfully.'
