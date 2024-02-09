6a9502e0ebf3b50e0a08c28a0e0af3f0913ab521
roandayne <roan.dino@sence1.com>
2024-02-09 19:25:10 +0800

    Add config files and env

--- frontend/src/axiosConfig.ts
+++ frontend/src/axiosConfig.ts
@@ -1,11 +1,13 @@
 import axios, { AxiosInstance } from 'axios';
 
+const baseURL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5001';
+
 const instance: AxiosInstance = axios.create({
-  baseURL: 'http://localhost:5001',
+  baseURL,
   timeout: 10000,
   headers: {
     'Content-Type': '*/*',
-    'Access-Control-Allow-Origin': 'http://localhost:5001',
+    'Access-Control-Allow-Origin': baseURL,
     'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
   },
 });
