b2ab8d791a37b6b0d0e9ee0297f9458ae173c13f
roandayne <roan.dino@sence1.com>
2024-02-13 02:31:40 +0800

    Solving CORS error

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins ENV["REACT_URL"] || 'http://localhost:3000' 
+      # origins ENV["REACT_URL"] || 'http://localhost:3000' 
+      origins '*'
       resource '/api/v1/*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
     end
   end
