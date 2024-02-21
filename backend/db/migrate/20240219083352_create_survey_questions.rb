1000bf6ad4f3c80573ff8bdf94a8cf0fbdd3e480
royfredeluces95 <roy.fredeluces@sence1.com>
2024-02-21 13:48:00 +0800

    reverted survey questions migration

--- backend/db/migrate/20240219083352_create_survey_questions.rb
+++
@@ -1,9 +0,0 @@
-class CreateSurveyQuestions < ActiveRecord::Migration[7.1]
-  def change
-    create_table :survey_questions do |t|
-      t.text :question_text, null: false
-      t.string :user_type, null: false
-      t.timestamps
-    end
-  end
-end
