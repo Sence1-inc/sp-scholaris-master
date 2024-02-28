18df6d4a5b73f67290f3c663af14d17d5a8b2fe7
sence1-rachelle <rachelle.gregorio@sence1.com>
2024-02-28 14:02:49 +0800

    reusable thank you page

--- frontend/src/containers/ThankYouPage/ThankYouPage.tsx
+++ frontend/src/containers/ThankYouPage/ThankYouPage.tsx
@@ -1,35 +1,41 @@
-import React from 'react';
-import './ThankYouPage.css';
+import React from "react";
+import "./ThankYouPage.css";
 // import WelcomePage from '/containers/WelcomePage/WelcomePage';
-import { Box, Button, Container } from '@mui/material';
+import { Box, Button, Container } from "@mui/material";
 // import { amber } from '@mui/material/colors';
 
 const ThankYouPage: React.FC = () => {
-    return (
-      <>
-        <Box className='content__thank-you'>
-            <Container className='container'>
-                <h2>Thank you for your Feedback!</h2>
-                <h3>We're grateful for your feedback</h3>
-                <p>We will keep on improving our services and feature.</p>
-                <Button 
-                    variant="contained"
-                    sx={{ 
-                        bgcolor: '#f36b3b',
-                        borderRadius: '16px',
-                        marginTop: 5,
-                        maxWidth: 'md', width: '100%', height: '70px',
-                        fontSize: '24px', fontFamily: 'Outfit', fontWeight: '700',
-                        ":hover": {
-                          bgcolor: "#002147",
-                          color: "white",
-                     },
-                    }}>Go Back Home
-                </Button>
-            </Container>
-        </Box>
-      </>
-    );
-  }
-  
-  export default ThankYouPage;
+  return (
+    <>
+      <Box className="content__thank-you">
+        <Container className="container">
+          <h2>Thank you for your Feedback!</h2>
+          <h3>We're grateful for your feedback</h3>
+          <p>We will keep on improving our services and feature.</p>
+          <Button
+            variant="contained"
+            sx={{
+              bgcolor: "#f36b3b",
+              borderRadius: "16px",
+              marginTop: 5,
+              maxWidth: "md",
+              width: "100%",
+              height: "70px",
+              fontSize: "24px",
+              fontFamily: "Outfit",
+              fontWeight: "700",
+              ":hover": {
+                bgcolor: "#002147",
+                color: "white",
+              },
+            }}
+          >
+            Go Back Home
+          </Button>
+        </Container>
+      </Box>
+    </>
+  );
+};
+
+export default ThankYouPage;
