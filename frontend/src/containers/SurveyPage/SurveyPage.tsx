345f3b814a49ff219469a1d50615a25949e8b4db
christian-barrios995 <156179390+christian-barrios995@users.noreply.github.com>
2024-02-29 12:00:46 +0800

    removed comments

--- frontend/src/containers/SurveyPage/SurveyPage.tsx
+++ frontend/src/containers/SurveyPage/SurveyPage.tsx
@@ -1,64 +1,3 @@
-// import React, { useState, useEffect, useRef } from "react";
-// import { useNavigate } from "react-router-dom";
-// import Button from "../../components/Button/Button";
-// import './SurveyPage.css';
-// import axios from "axios";
-
-// interface SurveyQuestion {
-//   id: number;
-//   question_text: string;
-// }
-
-// const SurveyPage: React.FC = () => {
-//   const [surveyQuestions, setSurveyQuestions] = useState<SurveyQuestion[] | null>(null);
-//   const [surveyNumber, setSurveyNumber] = useState(1);
-//   const navigate = useNavigate();
-//   const textarea = useRef<HTMLTextAreaElement | null>(null);
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const response = await axios.get(`http://localhost:5001/api/v1/survey_questions?user_type=${'student'}`);
-//         // Temporary
-//         setSurveyQuestions(response.data.survey_questions || []);
-//       } catch (error) {
-//         console.error("Error fetching survey questions:", error);
-//       }
-//     };
-
-//     fetchData();
-//   }, []);
-
-//   const handleClick = () => {
-//     if (surveyNumber < (surveyQuestions?.length || 0)) {
-//       setSurveyNumber(prev => prev + 1);
-//       textarea.current!.value = ''
-//     } else {
-//       navigate("/thank-you");
-//     }
-//   }
-
-//   const currentQuestion = surveyQuestions?.find(question => question.id === surveyNumber);
-
-//   return (
-//     <div className="survey">
-//       <div className="container-1040">
-//         <div className="survey__header-container">
-//           <h2>Survey</h2>
-//         </div>
-//         {currentQuestion && (
-//           <>
-//             <p className="survey-description" key={currentQuestion.id}>{currentQuestion.question_text}</p>
-//             <textarea ref={textarea}></textarea>
-//             <Button handleClick={handleClick}>Confirm</Button>
-//           </>
-//         )}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default SurveyPage;
 import React, { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { Button, Container, Typography, styled } from "@mui/material";
@@ -116,18 +55,18 @@ const SurveyPage: React.FC = () => {
 
   return (
     <Container maxWidth="md" sx={{ display: "flex", flexDirection: "column", rowGap: '50px', textAlign: "center", marginBlock: "40px" }}>
-      <div style={{ textAlign: "center", marginTop: "20px" }}>
-        <Typography variant="h2" style={{fontWeight: '700'}}>Survey</Typography>
-      </div>
+        <Typography variant="h2" sx={{fontWeight: '700', textAlign: "center", marginTop: "20px"}}>Survey</Typography>
       {currentQuestion && (
         <>
-          <Typography variant="body1" style={{fontSize: '24px', color: 'var(--primary-color)'}}>
+          <Typography variant="body1" sx={{fontSize: '24px', color: 'var(--primary-color)'}}>
             {currentQuestion.question_text}
           </Typography>
           <CustomTextarea
             ref={textarea}
           />
-          <Button variant="contained" color="primary" onClick={handleClick} style={{borderRadius: '16px', backgroundColor: '#f36b3b', padding: '20px', marginBottom: '20px'}}>
+          <Button variant="contained" color="primary" onClick={handleClick} sx={{borderRadius: '16px', backgroundColor: '#f36b3b', padding: '20px', marginBottom: '20px', "&:hover": {
+                backgroundColor: '#d2522b', // Change to the desired hover color
+              }}}>
             Confirm
           </Button>
         </>
