b0b021ad85f12c1657fe2a56a1606e1f915144b5
roandayne <roan.dino@sence1.com>
2024-02-29 16:16:38 +0800

    Set axios to use localhost for development

--- frontend/src/axiosConfig.ts
+++ frontend/src/axiosConfig.ts
@@ -1,15 +1,18 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosInstance } from "axios";
 
-const baseURL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5001';
+const baseURL =
+  process.env.NODE_ENV !== "development"
+    ? process.env.REACT_APP_API_BASE_URL
+    : "http://localhost:5001";
 
 const instance: AxiosInstance = axios.create({
   baseURL,
   timeout: 10000,
   headers: {
-    'Content-Type': ['application/json', '*/*'],
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
-    'Access-Control-Allow-Headers': '*'
+    "Content-Type": ["application/json", "*/*"],
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+    "Access-Control-Allow-Headers": "*",
   },
 });
 
