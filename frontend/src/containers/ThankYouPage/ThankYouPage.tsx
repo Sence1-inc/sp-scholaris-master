502d127c57a72e2788a347421569994f47da053c
sence1-rachelle <rachelle.gregorio@sence1.com>
2024-02-29 11:52:58 +0800

    revisions based on feedback

--- frontend/src/containers/ThankYouPage/ThankYouPage.tsx
+++ frontend/src/containers/ThankYouPage/ThankYouPage.tsx
@@ -1,29 +1,29 @@
 import React from 'react';
 import './ThankYouPage.css';
 // import WelcomePage from '/containers/WelcomePage/WelcomePage';
-import { Button } from '@mui/material';
-import { amber } from '@mui/material/colors';
+import { Box, Button, Container } from '@mui/material';
+// import { amber } from '@mui/material/colors';
 
 const ThankYouPage: React.FC = () => {
     return (
       <>
-        <div className='content__thank-you'>
-            <div className='container'>
+        <Box className='content__thank-you'>
+            <Container className='container'>
                 <h2>Thank you for your Feedback!</h2>
                 <h3>We're grateful for your feedback</h3>
                 <p>We will keep on improving our services and feature.</p>
                 <Button 
                     variant="contained"
                     sx={{ 
-                        bgcolor: amber[900],
+                        bgcolor: '#f36b3b',
                         borderRadius: '16px',
                         marginTop: 5,
                         maxWidth: 'md', width: '100%', height: '70px',
                         fontSize: '24px', fontFamily: 'Outfit', fontWeight: '700'
                     }}>Go Back Home
                 </Button>
-            </div>
-        </div>
+            </Container>
+        </Box>
       </>
     );
   }
