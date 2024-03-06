e1c98d055acc1c2e1295e030aa5d053750ba9bec
roandayne <roan.dino@sence1.com>
2024-03-06 12:55:36 +0800

    Float the newsletter section

--- frontend/src/components/Newsletter/Newsletter.tsx
+++ frontend/src/components/Newsletter/Newsletter.tsx
@@ -1,9 +1,12 @@
-import React, { useEffect, useRef, useState } from 'react'
-import Button from '../Button/Button'
-import Input from '../Input/Input'
-import { Link, useLocation } from 'react-router-dom'
-import './Newsletter.css'
+import Alert from '@mui/material/Alert'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Container from '@mui/material/Container'
+import TextField from '@mui/material/TextField'
+import Typography from '@mui/material/Typography'
 import { AxiosResponse } from 'axios'
+import React, { useEffect, useRef, useState } from 'react'
+import { Link, useLocation } from 'react-router-dom'
 import axiosInstance from '../../axiosConfig'
 
 interface SubscriberData {
@@ -40,7 +43,7 @@ const Newsletter: React.FC<NewsletterProps> = ({
   const [successMessage, setSuccessMessage] = useState<string>('')
   const [hasScrolled, setHasScrolled] = useState(false)
   const { hash } = useLocation()
-  const newletterRef = useRef<HTMLElement>(null)
+  const newletterRef = useRef<HTMLDivElement>(null)
 
   const handleSubscribe: (
     e: React.MouseEvent<HTMLButtonElement>
@@ -97,36 +100,87 @@ const Newsletter: React.FC<NewsletterProps> = ({
   }, [newletterRef, hash])
 
   return (
-    <section ref={newletterRef} id="newsletter" className="newsletter">
-      <div className="container-1040">
-        <h2 className="newsletter-subheader">{title_content}</h2>
-        <div className="section-header">
-          <h3>{subtitle_content}</h3>
-          <p>{description_content}</p>
-        </div>
-        <div className="newsletter-input__container">
-          <Input
+    <Box ref={newletterRef} id="newsletter">
+      <Container maxWidth="lg">
+        <Typography
+          variant="h3"
+          align="center"
+          sx={{
+            mb: 2,
+            color: 'var(--primary-color)',
+            textAlign: 'center',
+            fontWeight: '700',
+            fontSize: '48px',
+          }}
+        >
+          {title_content}
+        </Typography>
+        <Box sx={{ mb: 4 }}>
+          <Typography
+            variant="h4"
+            sx={{
+              mb: 1,
+              color: 'var(--secondary-color)',
+              textAlign: 'center',
+              fontWeight: '700',
+            }}
+          >
+            {subtitle_content}
+          </Typography>
+          <Typography
+            sx={{ fontSize: '24px', fontWeight: '400', textAlign: 'center' }}
+            variant="body1"
+          >
+            {description_content}
+          </Typography>
+        </Box>
+        <Box sx={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+          <TextField
+            sx={{
+              '& fieldset': { border: 'none' },
+              borderRadius: '16px',
+              border: '1px solid #0E2F71',
+            }}
+            id="outlined-basic"
+            variant="outlined"
             value={email}
-            placeholder={'Enter your email'}
-            handleChange={handleEmailChange}
+            onChange={handleEmailChange}
           />
-          {errorMessage && (
-            <p style={{ color: 'red', margin: 0 }}>{errorMessage}</p>
-          )}
-          {successMessage && (
-            <p style={{ color: 'green', margin: 0 }}>{successMessage}</p>
-          )}
-          <Button handleClick={handleSubscribe}>SUBSCRIBE</Button>
-          <p className="newsletter-text__small">
+          {errorMessage && <Alert severity="error">{errorMessage}</Alert>}
+          {successMessage && <Alert severity="success">{successMessage}</Alert>}
+          <Button
+            fullWidth
+            sx={{
+              padding: '20px',
+              borderRadius: '16px',
+              fontWeight: '700',
+              textTransform: 'capitalize',
+              backgroundColor: 'var(--secondary-color)',
+              color: 'white',
+              '&:hover': {
+                backgroundColor: 'var(--primary-color)',
+              },
+            }}
+            onClick={handleSubscribe}
+          >
+            SUBSCRIBE
+          </Button>
+          <Typography variant="body2" className="newsletter-text__small">
             By subscribing to the newsletter, I have read this form and
             understand its content and voluntarily give my consent for the
             collection, use, processing, storage and retention of my personal
-            data or information to Sence1 for the purpose(s) described in the
-            <Link to={'/privacy-consent'}> Privacy Policy</Link> document
-          </p>
-        </div>
-      </div>
-    </section>
+            data or information to Sence1 for the purpose(s) described in the{' '}
+            <Link
+              style={{ color: 'var(--primary-color)' }}
+              to={'/privacy-consent'}
+            >
+              Privacy Policy
+            </Link>{' '}
+            document
+          </Typography>
+        </Box>
+      </Container>
+    </Box>
   )
 }
 
--- frontend/src/containers/TeaserPage/TeaserStudent.tsx
+++ frontend/src/containers/TeaserPage/TeaserStudent.tsx
@@ -1,5 +1,8 @@
-import React from 'react'
+import EmailIcon from '@mui/icons-material/Email'
+import { Box, Fab } from '@mui/material'
+import React, { useState } from 'react'
 import FeatureGuides from '../../components/Feature/FeatureGuides'
+import FloatingElement from '../../components/FloatingElement/FloatingElement'
 import Newsletter from '../../components/Newsletter/Newsletter'
 import Search from '../../components/Search/Search'
 import WelcomeSection from '../../components/WelcomeSection/WelcomeSection'
@@ -11,25 +14,59 @@ import {
 } from '../../data/StudentContent'
 
 const HomePage: React.FC = () => {
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null)
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
   return (
     <>
       <WelcomeSection
         subheader={STUDENT_WELCOME_SUBHEADER}
         third_level_header={STUDENT_WELCOME_THIRD_LEVEL_HEADING}
       />
-      <Newsletter
-        user_type={STUDENT_TYPE}
-        title_content={
-          <>
-            Hear the latest from{' '}
-            <span className="newsletter-subheader__orange">Scholaris</span>
-          </>
-        }
-        subtitle_content={'Sign up for our newsletter'}
-        description_content={'Get the latest news about exciting new features'}
-      />
       <FeatureGuides features={FEATURES} contentType="studentFeatures" />
       <Search isSection />
+      <FloatingElement anchorEl={anchorEl} handleClose={handleClose}>
+        <Newsletter
+          user_type={STUDENT_TYPE}
+          title_content={
+            <>
+              Hear the latest from{' '}
+              <Box sx={{ color: 'var(--secondary-color)' }} component="span">
+                Scholaris
+              </Box>
+            </>
+          }
+          subtitle_content={'Sign up for our newsletter'}
+          description_content={
+            'Get the latest news about exciting new features'
+          }
+        />
+      </FloatingElement>
+      <Fab
+        onClick={handleClick}
+        variant="extended"
+        sx={{
+          position: 'fixed',
+          bottom: '30px',
+          right: '30px',
+          backgroundColor: 'var(--secondary-color)',
+          color: 'white',
+          padding: '20px',
+          '&:hover': {
+            backgroundColor: 'var(--primary-color)',
+          },
+        }}
+      >
+        <EmailIcon sx={{ mr: 1 }} />
+        Subscribe to our Newsletter
+      </Fab>
     </>
   )
 }
