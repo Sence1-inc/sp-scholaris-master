6da9aceee285e68cbc53bbfc674e32c313c0e533
sdidulosence1 <sarah.didulo@sence1.com>
2024-03-13 14:53:17 +0800

    2nd push

--- frontend/src/components/DashboardTable/DashboardTable.tsx
+++ frontend/src/components/DashboardTable/DashboardTable.tsx
@@ -1,41 +1,45 @@
 import * as React from 'react';
-import { DataGrid } from '@mui/x-data-grid';
+import { DataGrid, GridActionsCellItem } from '@mui/x-data-grid';
 
 const columns = [
   { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-  },
+  { field: 'scholarshipName', headerName: 'Scholarship Name', width: 130 },
+  { field: 'startDate', headerName: 'Start Date', type: "date", width: 130 },
+  { field: 'endDate', headerName: 'End Date', type: "date", width: 130 },
+  { field: 'status', headerName: 'Status', width: 130 },
+  { field: 'actions', headerName: 'Actions', type: "actions", width: 130,
+  getActions: (params) => [
+    <GridActionsCellItem
+      icon={<DeleteIcon />}
+      label="Delete"
+      onClick={deleteUser(params.id)}
+    />,
+    <GridActionsCellItem
+      icon={<SecurityIcon />}
+      label="Toggle Admin"
+      onClick={toggleAdmin(params.id)}
+      showInMenu
+    />,
+    <GridActionsCellItem
+      icon={<FileCopyIcon />}
+      label="Duplicate User"
+      onClick={duplicateUser(params.id)}
+      showInMenu
+    />
+  ]
+  }
 ];
 
+/* sample data - move to a separate file */
 const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+  { id: 1, scholarshipName: 'Tulong Dunong Scholarship', startDate: new Date(2023, 12, 9), endDate: new Date(2024, 1, 10), status: "active", },
   { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
   { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
   { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
 ];
 
 export default function DataTable() {
-  return (
+  return ( 
     <div style={{ height: 400, width: '100%' }}>
       <DataGrid
         rows={rows}
