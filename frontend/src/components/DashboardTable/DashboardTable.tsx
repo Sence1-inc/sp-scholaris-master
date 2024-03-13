f1f550b11cc89cf5aa18b979e15b08668602adad
sdidulosence1 <sarah.didulo@sence1.com>
2024-03-13 15:26:31 +0800

    added actions

--- frontend/src/components/DashboardTable/DashboardTable.tsx
+++ frontend/src/components/DashboardTable/DashboardTable.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { DataGrid, GridActionsCellItem } from '@mui/x-data-grid';
+import { Button } from '@mui/material';
 
 const columns = [
   { field: 'id', headerName: 'ID', width: 70 },
@@ -8,23 +9,36 @@ const columns = [
   { field: 'endDate', headerName: 'End Date', type: "date", width: 130 },
   { field: 'status', headerName: 'Status', width: 130 },
   { field: 'actions', headerName: 'Actions', type: "actions", width: 130,
-  getActions: (params) => [
+  getActions: () => [ //add params
     <GridActionsCellItem
-      icon={<DeleteIcon />}
+      icon={<Button variant="contained" sx={{
+        fontFamily: "Open Sans",
+        fontSize:"10px",
+        borderRadius: "15px",
+        backgroundColor: "#06A5FF"
+    }}>View</Button>}
+      label="View"
+      // onClick={viewScholarship(params.id)}
+    />,
+    <GridActionsCellItem
+      icon={<Button variant="contained" sx={{
+        fontFamily: "Open Sans",
+        fontSize:"10px",
+        borderRadius: "15px",
+        backgroundColor: "#1F4BEA"
+    }}>Edit</Button>}
+      label="Edit"
+      // onClick={editScholarship(params.id)}
+    />,
+    <GridActionsCellItem
+      icon={<Button variant="contained" sx={{
+        fontFamily: "Open Sans",
+        fontSize:"10px",
+        borderRadius: "15px",
+        backgroundColor: "#F50F0F"
+    }}>Delete</Button>}
       label="Delete"
-      onClick={deleteUser(params.id)}
-    />,
-    <GridActionsCellItem
-      icon={<SecurityIcon />}
-      label="Toggle Admin"
-      onClick={toggleAdmin(params.id)}
-      showInMenu
-    />,
-    <GridActionsCellItem
-      icon={<FileCopyIcon />}
-      label="Duplicate User"
-      onClick={duplicateUser(params.id)}
-      showInMenu
+      // onClick={deleteScholarship(params.id)}
     />
   ]
   }
@@ -32,10 +46,9 @@ const columns = [
 
 /* sample data - move to a separate file */
 const rows = [
-  { id: 1, scholarshipName: 'Tulong Dunong Scholarship', startDate: new Date(2023, 12, 9), endDate: new Date(2024, 1, 10), status: "active", },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+  { id: 1, scholarshipName: 'Tulong Dunong Scholarship', startDate: new Date(2023, 12, 9), endDate: new Date(2024, 1, 10), status: "active" }, //actions are not included in the rows.
+  { id: 1, scholarshipName: 'Tulong Dunong Scholarship 2', startDate: new Date(2023, 12, 9), endDate: new Date(2024, 1, 10), status: "active" },
+  { id: 1, scholarshipName: 'Tulong Dunong Scholarship 3', startDate: new Date(2023, 12, 9), endDate: new Date(2024, 1, 10), status: "active" }
 ];
 
 export default function DataTable() {
