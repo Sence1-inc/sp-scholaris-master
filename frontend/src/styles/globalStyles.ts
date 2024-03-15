c81f83200a3b7601028c79e89541c244bebf8ede
roandayne <roan.dino@sence1.com>
2024-03-15 10:16:01 +0800

    Create primary and cta buttons

--- frontend/src/styles/globalStyles.ts
+++ frontend/src/styles/globalStyles.ts
@@ -1,11 +1,5 @@
-export const primaryButtonStyle = {
-  borderRadius: '16px',
-  padding: '20px',
-  backgroundColor: 'primary.main',
-  '&:hover': { backgroundColor: 'secondary.main' },
-  color: 'common.white',
-  height: '100%',
-}
+import { Button } from '@mui/material'
+import { styled } from '@mui/system'
 
 export const ctaButtonStyle = {
   borderRadius: '16px',
@@ -15,3 +9,21 @@ export const ctaButtonStyle = {
   color: 'common.white',
   height: '100%',
 }
+
+export const PrimaryButton = styled(Button)({
+  borderRadius: '16px',
+  padding: '20px',
+  backgroundColor: '#002147',
+  '&:hover': { backgroundColor: '#f36b3b' },
+  color: '#fff',
+  height: '100%',
+})
+
+export const CTAButton = styled(Button)({
+  borderRadius: '16px',
+  padding: '20px',
+  backgroundColor: '#f36b3b',
+  '&:hover': { backgroundColor: '#002147' },
+  color: '#fff',
+  height: '100%',
+})
