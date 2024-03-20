03361e3968a17a39b57ccb8a4d849342cf073082
roandayne <roan.dino@sence1.com>
2024-03-20 17:07:50 +0800

    Delete unnecessary import

--- frontend/src/containers/ProviderDashboardPage/ProviderDashboardPage.tsx
+++ frontend/src/containers/ProviderDashboardPage/ProviderDashboardPage.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Link, Typography } from '@mui/material'
+import { Box, Button, Typography } from '@mui/material'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import DashboardTable from '../../components/DashboardTable/DashboardTable'
