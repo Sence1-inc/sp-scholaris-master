69593bf280aca51c1b50ea7e8a813771bbaa5c96
roandayne <roan.dino@sence1.com>
2024-04-09 11:24:09 +0800

    Use access token

--- frontend/src/hooks/useGetScholarships.ts
+++ frontend/src/hooks/useGetScholarships.ts
@@ -11,15 +11,35 @@ interface ErrorResponse {
   details: string[]
 }
 
+const isAccessTokenSet = (): boolean => {
+  const cookies = document.cookie.split(';')
+  for (let i = 0; i < cookies.length; i++) {
+    const cookie = cookies[i].trim()
+    console.log(cookie)
+    if (cookie.startsWith('access_token=')) {
+      return true
+    }
+  }
+  return false
+}
+
 const useGetScholarships = () => {
   const dispatch = useAppDispatch()
   const navigate = useNavigate()
   const params = useAppSelector((state) => state.searchParams)
 
   const getScholarships = async (isRedirected = true) => {
+    console.log(isAccessTokenSet())
+    const endpoint = isAccessTokenSet()
+      ? 'api/v1/scholarships/provider_scholarships'
+      : 'api/v1/scholarships'
+
     try {
       const response: AxiosResponse<Scholarship[] | ErrorResponse> =
-        await axiosInstance.get(`api/v1/scholarships`, params)
+        await axiosInstance.get(endpoint, {
+          params: { ...params },
+          withCredentials: isAccessTokenSet(),
+        })
 
       if (response.status === 200) {
         dispatch(initializeScholarships(response.data as Scholarship[]))
