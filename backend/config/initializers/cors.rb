20782c87bdcb018eb0859ba9ab2dff3f55a01fe8
roandayne <roan.dino@sence1.com>
2024-04-09 11:19:44 +0800

    Show cookies in frontend

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,8 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      # origins ENV["REACT_URL"] || 'http://localhost:3000' 
-      origins '*'
-      resource '/api/v1/*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
-    end
+  allow do
+    origins ENV["REACT_URL"]
+    resource '/api/v1/*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
-  
+end
