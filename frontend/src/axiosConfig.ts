9238755e926059454a51d1e486a14fabc0a9684b
roandayne <roan.dino@sence1.com>
2024-04-09 11:20:22 +0800

    Show cookies in frontend

--- frontend/src/axiosConfig.ts
+++ frontend/src/axiosConfig.ts
@@ -13,7 +13,19 @@ const instance: AxiosInstance = axios.create({
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
     'Access-Control-Allow-Headers': '*',
+    'Access-Control-Allow-Credentials': true,
   },
 })
+
+instance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      axios.post('/api/v1/refresh')
+      console.log('Unauthorized, triggering /refresh endpoint')
+    }
+    return Promise.reject(error)
+  }
+)
 
 export default instance
