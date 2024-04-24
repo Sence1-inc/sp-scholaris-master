2a90d5d68e40581a1d5cd23c73499f3e21f3328b
roandayne <roan.dino@sence1.com>
2024-04-24 19:25:23 +0800

    Test

--- frontend/src/components/PrivateRoute/PrivateRoute.tsx
+++ frontend/src/components/PrivateRoute/PrivateRoute.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { Navigate } from 'react-router-dom'
 import axiosInstance from '../../axiosConfig'
 import { initializeIsAuthenticated } from '../../redux/reducers/IsAuthenticatedReducer'
-import { useAppDispatch } from '../../redux/store'
+import { useAppDispatch, useAppSelector } from '../../redux/store'
 
 interface PrivateRouteProps {
   component: any
@@ -13,11 +13,13 @@ const PrivateRoute: React.FC<PrivateRouteProps> = ({
 }) => {
   const dispatch = useAppDispatch()
   const [authenticated, setAuthenticated] = useState<boolean | null>(null)
+  const user = useAppSelector((state) => state.persistedReducer.user)
 
   useEffect(() => {
     const checkAuthentication = async () => {
       try {
         const response = await axiosInstance.get('/api/v1/check_token', {
+          params: { email: user.email_address },
           withCredentials: true,
         })
         setAuthenticated(response.data.valid)
