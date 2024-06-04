cec7c8e0385a20b349c49d9b7789df3bc7eff2c7
roandayne <roan.dino@sence1.com>
2024-06-04 18:17:10 +0800

    Use MUI datagrid in Search Results page

--- frontend/src/containers/SearchResultsPage/SearchResultsPage.tsx
+++ frontend/src/containers/SearchResultsPage/SearchResultsPage.tsx
@@ -1,17 +1,25 @@
+import { OpenInNew } from '@mui/icons-material'
 import ArrowBackIos from '@mui/icons-material/ArrowBackIos'
 import HomeIcon from '@mui/icons-material/Home'
-import { Button, Typography, useTheme } from '@mui/material'
+import { Box, Button, Typography, useTheme } from '@mui/material'
+import { DataGrid } from '@mui/x-data-grid'
 import Cookies from 'js-cookie'
 import queryString from 'query-string'
 import { useEffect, useState } from 'react'
-import { useNavigate, useSearchParams } from 'react-router-dom'
+import { Link, useNavigate, useSearchParams } from 'react-router-dom'
 import Search from '../../components/Search/Search'
-import Table from '../../components/Table/Table'
 import useGetScholarships from '../../hooks/useGetScholarships'
 import { initializeParams } from '../../redux/reducers/SearchParamsReducer'
 import { useAppDispatch, useAppSelector } from '../../redux/store'
 import { Scholarship } from '../../redux/types'
 import './SearchResultsPage.css'
+
+interface GridRowDef {
+  scholarshipName: string
+  startDate: Date
+  endDate: Date
+  provider: string
+}
 
 interface SearchResultsPageProps {
   isASection: boolean
@@ -36,20 +44,89 @@ export const SearchResultsPage: React.FC<SearchResultsPageProps> = ({
   const result: any = useAppSelector(
     (state) => state.persistedReducer.scholarships
   )
-  const [scholarships, setScholarships] = useState<Scholarship[]>([])
-  const [page, setPage] = useState<number>(1)
+  const [page, setPage] = useState<number>(0)
   const params = useAppSelector((state) => state.searchParams)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [pageSize, setPageSize] = useState<number>(10)
+  const [totalCount, setTotalCount] = useState<number>(10)
+  const [rowData, setRowData] = useState<GridRowDef[]>([])
+
+  const columns = [
+    {
+      field: 'scholarshipName',
+      headerName: 'Scholarship Name',
+      flex: 1.5,
+    },
+    { field: 'startDate', headerName: 'Start Date', type: 'date', flex: 1 },
+    { field: 'endDate', headerName: 'End Date', type: 'date', flex: 1 },
+    { field: 'provider', headerName: 'Organization', type: 'string', flex: 1 },
+    {
+      field: 'actions',
+      headerName: 'Actions',
+      type: 'actions',
+      flex: 1,
+      renderCell: (params: any) => renderActions(params),
+    },
+  ]
+
+  const renderActions = (params: any) => {
+    return (
+      <Box>
+        <Typography
+          color="secondary"
+          component={Link}
+          to={`/scholarships/${params.row.id}`}
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            gap: '8px',
+            width: '150px',
+          }}
+        >
+          View
+          <OpenInNew fontSize="small" />
+        </Typography>
+      </Box>
+    )
+  }
+
+  const formatScholarships = (data: Scholarship[]) => {
+    const row = data.map((scholarship: Scholarship) => {
+      return {
+        id: scholarship.id,
+        scholarshipName: scholarship.scholarship_name,
+        startDate: new Date(scholarship.start_date),
+        endDate: new Date(scholarship.due_date),
+        provider: scholarship.scholarship_provider.provider_name,
+      }
+    })
+    setIsLoading(false)
+    setRowData(row)
+  }
+
+  const handlePageChange = (par: { page: number; pageSize: number }) => {
+    setIsLoading(true)
+    setPage(par.page + 1)
+    setPageSize(par.pageSize)
+  }
 
   useEffect(() => {
-    setScholarships(result.scholarships.scholarships)
-  }, [result.scholarships.scholarships])
+    if (page > 0) {
+      dispatch(initializeParams({ ...params.params, page: page }))
+    }
+  }, [page])
 
   useEffect(() => {
-    if ((params?.params?.page as number) > 0) {
+    if (params.params.page) {
       getScholarships()
     }
-    // eslint-disable-next-line
-  }, [params.params?.page])
+  }, [params.params.page])
+
+  useEffect(() => {
+    formatScholarships(result.scholarships.scholarships)
+    setTotalCount(result.scholarships.total_count)
+  }, [result.scholarships.scholarships])
 
   useEffect(() => {
     if ((params?.params?.page as number) > result?.scholarships?.total_pages) {
@@ -63,20 +140,6 @@ export const SearchResultsPage: React.FC<SearchResultsPageProps> = ({
     }
     // eslint-disable-next-line
   }, [params.params.page, result.scholarships.total_pages])
-
-  const handleNext = () => {
-    if (scholarships.length === 10) {
-      setPage(page + 1)
-      dispatch(initializeParams({ ...params.params, page: page + 1 }))
-    }
-  }
-
-  const handlePrevious = () => {
-    if (page >= 2) {
-      setPage((prevPage) => prevPage - 1)
-      dispatch(initializeParams({ ...params.params, page: page - 1 }))
-    }
-  }
 
   useEffect(() => {
     const initialData = {
@@ -96,6 +159,10 @@ export const SearchResultsPage: React.FC<SearchResultsPageProps> = ({
     dispatch(initializeParams(initialData.params))
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [course, school, benefits, location, start_date, due_date, provider, name])
+
+  useEffect(() => {
+    dispatch(initializeParams({ ...params.params, limit: pageSize }))
+  }, [pageSize])
 
   useEffect(() => {
     const queryParams = queryString.stringify(params.params)
@@ -134,22 +201,55 @@ export const SearchResultsPage: React.FC<SearchResultsPageProps> = ({
         </Button>
         <Typography variant="h3">Search Results</Typography>
         <Search isSection={false} />
-        {window.innerWidth > theme.breakpoints.values.md ? (
-          <Table
-            total_count={result.scholarships.total_count}
-            total_pages={result.scholarships.total_pages}
-            page={page}
-            hasPagination={true}
-            handleNext={handleNext}
-            handlePrevious={handlePrevious}
-            scholarships={result.scholarships.scholarships}
-          />
-        ) : (
-          <Typography sx={{ textAlign: 'center' }}>
-            You can see the results on a larger display or switch to landscape
-            mode for better viewing.
-          </Typography>
-        )}
+        <DataGrid
+          localeText={{ noRowsLabel: 'No saved data' }}
+          rows={rowData}
+          rowCount={totalCount}
+          columns={columns}
+          onPaginationModelChange={handlePageChange}
+          initialState={{
+            pagination: {
+              paginationModel: { page: page, pageSize: 10 },
+            },
+          }}
+          pageSizeOptions={[5, 10]}
+          pagination
+          paginationMode="server"
+          loading={isLoading}
+          sx={{
+            height:
+              Array.isArray(result.scholarships.scholarships) &&
+              result.scholarships.scholarships?.length > 0
+                ? 'auto'
+                : 200,
+            '.MuiDataGrid-root': {
+              border: 'none',
+            },
+            '& .MuiDataGrid-columnHeaders': {
+              backgroundColor: '#AFC3D9',
+            },
+            '& .MuiDataGrid-footerContainer': {
+              backgroundColor: '#AFC3D9', // Change table header color
+            },
+            '& .MuiDataGrid-row': {
+              '&:nth-of-type(odd)': {
+                backgroundColor: '#E0EFFF', // Change background color of odd rows
+              },
+              '&:nth-of-type(even)': {
+                backgroundColor: '#E0EFFF', // Change background color of odd rows
+              },
+            },
+            '& .MuiDataGrid-overlay': {
+              zIndex: '20',
+            },
+            // borderRadius: '16px',
+            fontFamily: 'Outfit',
+            fontSize: {
+              xs: '12px',
+              md: '1rem',
+            },
+          }}
+        />
       </div>
     </section>
   )
