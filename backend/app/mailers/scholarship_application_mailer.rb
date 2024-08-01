7beba87dcf8bffd8e64d649c9964aebbc215ed95
roandayne <roan.dino@sence1.com>
2024-08-01 15:02:08 +0800

    Update content

--- backend/app/mailers/scholarship_application_mailer.rb
+++ backend/app/mailers/scholarship_application_mailer.rb
@@ -7,13 +7,8 @@ class ScholarshipApplicationMailer < ApplicationMailer
     @student_name = student_name
     @provider_name = provider_name
     @promotional_message = <<-PROMOTIONAL_MESSAGE
-      <p>The Scholaris team would also want to take this opportunity to highlight the benefits of partnering with Scholaris:</p>
-      <ul>
-        <li><strong>Increased Visibility:</strong> Our platform connects students from diverse backgrounds with scholarship opportunities, enhancing the reach and impact of your scholarship program.</li>
-        <li><strong>Streamlined Application Process:</strong> Our app will simplify the application process, ensuring that your scholarship reaches motivated and qualified candidates efficiently. This functionality will be shipped soon. Stay tuned!</li>
-        <li><strong>Targeted Outreach:</strong> Our app is designed to help students find scholarships that align with their goals, increasing the likelihood of a successful match.</li>
-      </ul>
-      <p>We encourage you to review the student's application and consider their intent letter. Should you have any questions or need further information, please don't hesitate to reach out.</p>
+      <p>We would also like to take this opportunity to introduce to you Scholaris.</p>
+      <p>Scholaris is a centralized platform for scholarship offerings. Our platform enables students to search and choose from a kaleidoscope of scholarships. Through Scholaris, you can seamlessly manage and post your scholarships, increasing the chances of attracting more scholarship applicants.</p>
     PROMOTIONAL_MESSAGE
 
     mail(to: recipient_email, cc: student_email, subject: "#{student_name} Intent to Apply for #{scholarship_name} through Scholaris App") do |format|
