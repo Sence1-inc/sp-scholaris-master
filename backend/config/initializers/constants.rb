3131d27f325c361312d495c997ad343a65994b3b
sence1roandayne <roan.dino@sence1.com>
2024-08-29 14:07:27 +0800

    Update content

--- backend/config/initializers/constants.rb
+++ backend/config/initializers/constants.rb
@@ -3,65 +3,22 @@ STUDENT_TYPE = 'student'.freeze
 
 STUDENT_NEWSLETTER_SUBJECT = "Thank you for subscribing to Scholaris!".freeze
 STUDENT_NEWSLETTER_CONTENT = "
-  <h1>Thank You for Subscribing to the <span class='color-secondary'>Scholaris</span> newsletter!</h1>
+  <p>Thank You for Subscribing to <span class='color-secondary'>Scholaris</span>!</p>
   <p>
     Stay ahead of the competition with exclusive access to the latest scholarships, application tips, and other exciting news delivered straight to your inbox.
   </p>
 
   <p>
-    <span class='color-secondary'>Scholaris</span> will revolutionize how students discover life-changing scholarships. Stay tuned for the following features in store for aspiring scholars like you:
-  </p>
-
-  <ul>
-    <li>A search portal for different kinds of scholarship opportunities:</li>
-    <ul>
-      <li>Academic</li>
-      <li>Sports</li>
-      <li>Creative</li>
-      <li>Grants</li>
-      <li>Financial</li>
-    </ul>
-    <li>
-      Search for scholarships using advanced filters such as benefits, courses, locations, application dates, schools, and providers to find scholarships that align with your academic and personal interests.
-    </li>
-    <li>Learn more about the Scholarship Granting Organization (SGO) you're applying to by viewing their profiles.</li>
-  </ul>
-
-  <p>
     Stay tuned for what's ahead in the next few months for <span class='color-secondary'>Scholaris</span>. We look forward to being part of your academic success story. Welcome to the <span class='color-secondary'>Scholaris</span> community!
   </p>
-
-  <section>
-    <p>Best regards, <br/>The Scholaris Team</p>
-    <hr/>
-    <p>For any issues, please contact us: <br/>
-      Email: <a href='mailto:scholaris-support@sence1.com'>scholaris-support@sence1.com</a> <br/>
-      Visit our website: <a href='https://scholaristest.sence1.com'>https://scholaristest.sence1.com</a>
-    </p>
-  </section>
 ".freeze
 
 PROVIDER_NEWSLETTER_SUBJECT = "Thank you for subscribing to Scholaris!".freeze
 PROVIDER_NEWSLETTER_CONTENT = "
-  <h1>Thank you for signing up to our newsletter! You will now be receiving the latest updates from <span class='color-secondary'>Scholaris</span>!</h1>
+  <p>Thank you for subscribing to <span class='color-secondary'>Scholaris</span>, a service that will revolutionize the way scholarship programs are implemented.</p>
 
   <p>
-    <span class='color-secondary'>Scholaris</span> is a service that will revolutionize the way scholarship programs are implemented. This service will aid you in finding the best candidate for your scholarships, matching their inclinations, aptitudes, and competencies that suit your organization’s vision.
-  </p>
-
-  <p>
-    For Scholarship Granting Organizations, we have the following features in store for you in the future:
-  </p>
-
-  <ul>
-    <li>List scholarships open for applications</li>
-    <li>Manage scholarship applications</li>
-    <li>Search for your next scholar</li>
-    <li>Invite students to apply for your scholarship</li>
-  </ul>
-
-  <p>
-    There are also plans to streamline the experience of supervising scholarships through a management system where you will have more control in managing scholars, providing them with support, and disbursing scholarship grants.
+    This service will aid you in finding the best candidate for your scholarships: matching their inclinations, aptitudes, and competencies that suit your organization’s vision.
   </p>
 
   <p>
@@ -69,19 +26,10 @@ PROVIDER_NEWSLETTER_CONTENT = "
   </p>
 
   <p>
-    If you would like to help us improve our service functionality, please reply to this email. We will send you a message from here. We are considering special benefits in the future for those who cooperate with the interview. Thank you very much for your support.
+    Should you be interested, kindly inform us at scholaris-support@sence1.com
   </p>
 
   <p>
     Stay tuned for what's ahead in the next few months for <span class='color-secondary'>Scholaris</span>. We are proud that you will become part of something revolutionary for the education scene!
   </p>
-
-  <section>
-    <p>Best regards, <br/>The Scholaris Team</p>
-    <hr/>
-    <p>For any issues, please contact us: <br/>
-      Email: <a href='mailto:scholaris-support@sence1.com'>scholaris-support@sence1.com</a> <br/>
-      Visit our website: <a href='https://scholaristest.sence1.com'>https://scholaristest.sence1.com</a>
-    </p>
-  </section>
 ".freeze
