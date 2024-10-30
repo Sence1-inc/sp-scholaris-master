d9854eb4ec86650c8f534ab83e6193622baf9e4e
sence1roandayne <roan.dino@sence1.com>
2024-10-30 13:57:59 +0800

    Make the editable columns more obvious

--- frontend/src/containers/ApplicationsManagementPage/ApplicationsManagementPage.tsx
+++ frontend/src/containers/ApplicationsManagementPage/ApplicationsManagementPage.tsx
@@ -101,6 +101,13 @@ const ApplicationsManagementPage = () => {
       type: 'string',
       editable: isEditable,
       flex: 1,
+      renderHeader: () => {
+        return isEditable ? (
+          <span style={{ backgroundColor: '#f36b3b' }}>Update the Status</span>
+        ) : (
+          <span>Status</span>
+        )
+      },
       renderCell: (params: GridRenderCellParams) => renderStatus(params),
     },
     {
@@ -116,6 +123,17 @@ const ApplicationsManagementPage = () => {
       type: 'string',
       editable: isEditable,
       flex: 1,
+      renderHeader: () => {
+        return isEditable ? (
+          <span style={{ backgroundColor: '#f36b3b' }}>Edit your Notes</span>
+        ) : (
+          <span>Notes</span>
+        )
+      },
+      renderCell: (params: GridRenderCellParams) =>
+        params.value ?? (
+          <span style={{ color: 'lightgray' }}>Add your notes here</span>
+        ),
     },
     {
       field: 'actions',
