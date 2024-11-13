470c722f41cd2df719c936357e9cb9e4b41ea8a8
sence1roandayne <roan.dino@sence1.com>
2024-11-13 17:03:18 +0800

    Add constants for admin

--- frontend/src/constants/constants.ts
+++ frontend/src/constants/constants.ts
@@ -1,7 +1,9 @@
 export const PROVIDER_TYPE: string = 'provider'
 export const STUDENT_TYPE: string = 'student'
+export const ADMIN_TYPE: string = 'admin'
 export const STUDENT_ROLE_ID: number = 3
 export const PROVIDER_ROLE_ID: number = 4
+export const ADMIN_ROLE_ID: number = 5
 
 // FOR NEW STATUSES, MAKE SURE TO UPDATE BACKEND CONSTANT VALUES AS WELL
 export const APPLICATION_STATUSES: { [key: number]: string } = {
