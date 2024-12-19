a23547e60eeac635f841407ffe71cd136c74abf0
sence1-rachelle <rachelle.gregorio@sence1.com>
2024-12-19 16:48:56 +0800

    sign in button enter key function

--- frontend/src/containers/SignInPage/SignInPage.tsx
+++ frontend/src/containers/SignInPage/SignInPage.tsx
@@ -144,13 +144,7 @@ const SignInPage: React.FC<SignInPageProps> = () => {
       }
     }
   }
-
-  const handleKeyDown = (e: { key: string }) => {
-    if (e.key === "Enter") {
-      handleSignIn();
-    }
-  };
-
+  
   return (
     <Container
       maxWidth="md"
@@ -175,7 +169,7 @@ const SignInPage: React.FC<SignInPageProps> = () => {
       <CustomTextfield
         label="Email address"
         value={userCredentials.email_address.toLowerCase()}
-        onKeyDown={handleKeyDown}
+        handleOnKeyDonw={handleSignIn}
         handleChange={(e: React.ChangeEvent<HTMLInputElement>) =>
           handleEmail(e.target.value)
         }
@@ -186,7 +180,7 @@ const SignInPage: React.FC<SignInPageProps> = () => {
         type="password"
         label="Password"
         value={userCredentials.password}
-        onKeyDown={handleKeyDown}
+        handleOnKeyDonw={handleSignIn}
         handleChange={(e: React.ChangeEvent<HTMLInputElement>) =>
           handlePassword(e.target.value)
         }
