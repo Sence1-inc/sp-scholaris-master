4fba58a6674a0e03ca3e5dea4f42eee564c7a381
royfredeluces95 <roy.fredeluces@sence1.com>
2024-12-23 14:47:28 +0800

    added optional deleted_at param to subscribe reducer

--- frontend/src/redux/reducers/SubscriberReducer.ts
+++ frontend/src/redux/reducers/SubscriberReducer.ts
@@ -3,11 +3,13 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 interface Subscriber {
   email: string
   user_type: string
+  deleted_at?: string | null
 }
 
 const initialState: Subscriber = {
   email: '',
   user_type: '',
+  deleted_at: null,
 }
 
 export const subscriberSlice = createSlice({
