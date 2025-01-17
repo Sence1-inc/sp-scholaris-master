32e0abbcf7f6d308f852687b2b0ab2786968b037
royfredeluces95 <roy.fredeluces@sence1.com>
2025-01-17 17:38:54 +0800

    added model method is_bookmarked

--- backend/app/models/bookmark.rb
+++ backend/app/models/bookmark.rb
@@ -4,5 +4,14 @@ class Bookmark < ApplicationRecord
     belongs_to :scholarship
   
     default_scope -> { where(deleted_at: nil) }
+    
+
+    def self.is_bookmarked(user_id, scholarship_id)
+        if self.find_by(user_id: user_id, scholarship_id: scholarship_id)
+            return true
+        else
+            return false
+        end
+    end
 
 end
