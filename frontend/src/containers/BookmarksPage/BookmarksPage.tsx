8b75ed97ca59aa440602cce798a7b436b57838f9
sdidulosence1 <sarah.didulo@sence1.com>
2025-01-21 12:24:02 +0800

    fix in error message in bookmarks page

--- frontend/src/containers/BookmarksPage/BookmarksPage.tsx
+++ frontend/src/containers/BookmarksPage/BookmarksPage.tsx
@@ -240,7 +240,7 @@ const BookmarksPage: React.FC = () => {
       const response = await axiosInstance.get(`/api/v1/bookmarks/${user.id}`)
       formatScholarships(response.data.scholarships)
     } catch (error: any) {
-      showMessage(error.response.data.error, 'Error')
+      showMessage(error.response.data.error, 'error')
     }
   }
 
