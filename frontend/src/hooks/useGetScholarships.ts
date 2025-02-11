32a50dfab75cce7b9e0526ebae3cd8bdbc62f823
sence1roandayne <roan.dino@sence1.com>
2025-02-11 17:09:35 +0800

    Use initial user state from user reducer instead of from axios config

--- frontend/src/hooks/useGetScholarships.ts
+++ frontend/src/hooks/useGetScholarships.ts
@@ -2,7 +2,7 @@ import { AxiosResponse } from 'axios'
 import queryString from 'query-string'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import axiosInstance, { baseURL } from '../axiosConfig'
+import axiosInstance, { baseURL, CustomApiError } from '../axiosConfig'
 import {
   initializeScholarships,
   Scholarships,
@@ -42,9 +42,8 @@ const useGetScholarships = () => {
       }
     } catch (error) {
       setAreScholarshipsLoading(false)
-      if (error) {
+      if (error instanceof CustomApiError) {
         dispatch(initializeScholarships([]))
-        console.error('Error: ', error)
       }
     }
   }
