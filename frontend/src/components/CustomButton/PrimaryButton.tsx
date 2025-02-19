2853f38c8b790e88f19f60bd6bb3c846af45fb7b
sence1roandayne <roan.dino@sence1.com>
2025-02-19 09:16:11 +0800

    Add TSDoc to CustomButtons

--- frontend/src/components/CustomButton/PrimaryButton.tsx
+++ frontend/src/components/CustomButton/PrimaryButton.tsx
@@ -1,6 +1,16 @@
 import { LoadingButton } from '@mui/lab'
 import React from 'react'
 
+/**
+ * Props for the {@link PrimaryButton} component.
+ *
+ * @property {function} handleClick - The function to call when the button is clicked.
+ * @property {string} label - The label to display on the button.
+ * @property {boolean} loading - Indicates if the button is in a loading state.
+ * @property {any} styles - The styles to apply to the button.
+ * @property {boolean} disabled - Indicates if the button is disabled.
+ * @property {string} id - The id of the button.
+ */
 interface PrimaryButtonProps {
   handleClick: any
   label: string
@@ -10,6 +20,12 @@ interface PrimaryButtonProps {
   id?: string
 }
 
+/**
+ * A primary button component that displays a label and handles click events.
+ *
+ * @param {PrimaryButtonProps} props - The properties for the PrimaryButton component.
+ * @returns {JSX.Element} The rendered PrimaryButton component.
+ */
 const PrimaryButton: React.FC<PrimaryButtonProps> = ({
   handleClick,
   label,
